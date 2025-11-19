d7faa41a27d455d6240bc1524d94cf30304669ab
Ari-42 <ari.0.hcj@gmail.com>
2025-11-19 12:47:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ function toggleMenu() {
     nav.classList.toggle("open");
     hamburger.classList.toggle("active");
 }
+const images = [
+    "Imagenes/cocina1.jpeg",
+    "Imagenes/dormitorio1-1.jpg",
+    "Imagenes/dormitorio2-1.jpg",
+    "Imagenes/dormitorio3-1.jpg",
+    "Imagenes/dormitorio4-1.jpg",
+    "Imagenes/bano1-1.jpg",
+    "Imagenes/bano2-1.jpg"
+];
+const images2 = [
+    "Imagenes/salon-Comedor2.jpg",
+    "Imagenes/cocina2.jpg",
+    "Imagenes/dormitorio1-2.jpg",
+    "Imagenes/dormitorio2-2.jpg",
+    "Imagenes/bano2.jpg"
+];
+const images3 = [
+    "Imagenes/reuniones.jpg",
+    "Imagenes/social.jpg",
+    "Imagenes/almacen.jpg",
+    "Imagenes/oficina-administrativa.jpg"
+];
